d2479638979e7eca2794abea340da0ba79579003
nfrancken@gmail.com <nfrancken@gmail.com@950b781c-c544-efab-2f99-90083ee69683>
2012-04-19 05:01:55 +0000

    MINOR
    git-svn-id: https://silverstripe-ecommerce.googlecode.com/svn/modules/ecommerce_alsorecommended/trunk@2488 950b781c-c544-efab-2f99-90083ee69683

--- code/model/EcommerceAlsoRecommendedDOD.php
+++ code/model/EcommerceAlsoRecommendedDOD.php
@@ -19,11 +19,21 @@ class EcommerceAlsoRecommendedDOD extends DataObjectDecorator {
 	function updateCMSFields(FieldSet &$fields) {
 		if($this->owner instanceOf Product) {
 			$field = new TreeMultiselectField ("EcommerceRecommendedProducts", "Recommended Products", $sourceObject = "SiteTree", $keyField = "ID", $labelField = "Title");
-			$filter = create_function('$obj', 'return ( ( $obj InstanceOf Product) && ($obj->ID != '.$this->owner->ID.'));');
+			$filter = create_function('$obj', 'return ( ( $obj InstanceOf Product || $obj InstanceOf ProductGroup) && ($obj->ID != '.$this->owner->ID.'));');
 			$field->setFilterFunction($filter);
 			$fields->addFieldToTab('Root.Content.RecommendedProducts', $field);
 		}
 	}
 
+	function onBeforeWrite(){
+		$products = $this->owner->EcommerceRecommendedProducts();
+		if($products) {
+			foreach($products as $product) {
+				if(!$product instanceOf Product) {
+					$products->remove($product);
+				}
+			}
+		}
+	}
 
 }
