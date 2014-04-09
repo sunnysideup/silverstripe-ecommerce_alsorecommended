57946da1da2b5aada95d1a60791debe22d8d3c65
nfrancken@gmail.com <nfrancken@gmail.com@950b781c-c544-efab-2f99-90083ee69683>
2014-04-09 05:12:49 +0000

    UPGRADE 3.1
    git-svn-id: https://silverstripe-ecommerce.googlecode.com/svn/modules/ecommerce_alsorecommended/trunk@4932 950b781c-c544-efab-2f99-90083ee69683

--- _config.php
+++ _config.php
@@ -3,11 +3,18 @@
 
 /**
  * @author Nicolaas modules [at] sunnysideup.co.nz
- *
- **/
 
-//copy the lines between the START AND END line to your /mysite/_config.php file and choose the right settings
-//===================---------------- START ecommerce_alsorecommended MODULE ----------------===================
-//Object::add_extension('Product', 'EcommerceAlsoRecommendedDOD');
-// do not forget to add the modifier to the array of modifers, in case you want to use it.
-//===================---------------- END ecommerce_alsorecommended MODULE ----------------===================
+	 do not forget to add the modifier to the array of modifers, in case you want to use it:
+
+	 in mysite/_config.php:
+	 Config::inst()->update("Order", "modifiers", "RecommendedProductsModifier");
+
+	 or BETTER, in mysite/_config/config.yml (or similar):
+
+	Order:
+		modifiers: [
+			...
+			RecommendedProductsModifier
+		]
+*/
+
