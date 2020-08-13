369074d65fe07a27a3f751e4f55d1ef342c0e3fd
Nicolaas Thiemen Francken <github@sunnysideup.co.nz>
2020-08-13 22:50:01 +1200

    PATCH: fix

--- _config.php
+++ _config.php
@@ -7,7 +7,7 @@
      do not forget to add the modifier to the array of modifers, in case you want to use it:
 
      in mysite/_config.php:
-     Config::inst()->update("Order", "modifiers", "RecommendedProductsModifier");
+     Config::modify()->update("Order", "modifiers", "RecommendedProductsModifier");
 
      or BETTER, in mysite/_config/config.yml (or similar):
 
