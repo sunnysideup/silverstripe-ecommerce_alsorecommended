c3d94d15d060a7c413304eba98ad3bd555bbe757
nicolaas @ tappy <github@sunnysideup.co.nz>
2021-02-09 08:32:44 +1300

    PATCH: LINTER

--- client/javascript/RecommendedProductsModifier.js
+++ client/javascript/RecommendedProductsModifier.js
@@ -6,58 +6,58 @@
 **/
 
 (function($){
-	$(document).ready(
-		function() {
-			RecommendedProductsModifier.init();
-		}
-	);
+    $(document).ready(
+        function() {
+            RecommendedProductsModifier.init();
+        }
+    );
 })(jQuery);
 
 var RecommendedProductsModifier = {
 
-	formID: "RecommendedProductsModifierForm_RecommendedProducts",
+    formID: "RecommendedProductsModifierForm_RecommendedProducts",
 
-	willBeAddedToCartClass: "willBeAddedToCart",
+    willBeAddedToCartClass: "willBeAddedToCart",
 
-	willNotBeAddedToCartClass: "willNotBeAddedToCart",
+    willNotBeAddedToCartClass: "willNotBeAddedToCart",
 
-	reloadCheckoutPage: true,
+    reloadCheckoutPage: true,
 
 
-	init: function() {
-		if(RecommendedProductsModifier.reloadCheckoutPage) {
-			jQuery("body").on(
-				"click",
-				"#" + RecommendedProductsModifier.formID + " .productActions a",
-				function() {
-					window.setTimeout(
-						function() {
-							location.reload();
-						},
-						500
-					);
-				}
-			);
-		}
-		else {
-			jQuery("#" + RecommendedProductsModifier.formID + " .checkbox input").addClass(RecommendedProductsModifier.willNotBeAddedToCartClass);
-			RecommendedProductsModifier.changeOnTick();
-		}
-	},
+    init: function() {
+        if(RecommendedProductsModifier.reloadCheckoutPage) {
+            jQuery("body").on(
+                "click",
+                "#" + RecommendedProductsModifier.formID + " .productActions a",
+                function() {
+                    window.setTimeout(
+                        function() {
+                            location.reload();
+                        },
+                        500
+                    );
+                }
+            );
+        }
+        else {
+            jQuery("#" + RecommendedProductsModifier.formID + " .checkbox input").addClass(RecommendedProductsModifier.willNotBeAddedToCartClass);
+            RecommendedProductsModifier.changeOnTick();
+        }
+    },
 
-	changeOnTick: function() {
-		jQuery("#" + RecommendedProductsModifier.formID + " .checkbox input").change(
-			function() {
-				if(jQuery(this).is(":checked")) {
-					jQuery(this).parent().addClass(RecommendedProductsModifier.willBeAddedToCartClass);
-					jQuery(this).parent().removeClass(RecommendedProductsModifier.willNotBeAddedToCartClass);
-				}
-				else {
-					jQuery(this).parent().addClass(RecommendedProductsModifier.willNotBeAddedToCartClass);
-					jQuery(this).parent().removeClass(RecommendedProductsModifier.willBeAddedToCartClass);
-				}
-			}
-		);
-	}
+    changeOnTick: function() {
+        jQuery("#" + RecommendedProductsModifier.formID + " .checkbox input").change(
+            function() {
+                if(jQuery(this).is(":checked")) {
+                    jQuery(this).parent().addClass(RecommendedProductsModifier.willBeAddedToCartClass);
+                    jQuery(this).parent().removeClass(RecommendedProductsModifier.willNotBeAddedToCartClass);
+                }
+                else {
+                    jQuery(this).parent().addClass(RecommendedProductsModifier.willNotBeAddedToCartClass);
+                    jQuery(this).parent().removeClass(RecommendedProductsModifier.willBeAddedToCartClass);
+                }
+            }
+        );
+    }
 
 }
--- templates/Sunnysideup/EcommerceAlsoRecommended/EcommerceRecommendedProducts.ss
+++ templates/Sunnysideup/EcommerceAlsoRecommended/EcommerceRecommendedProducts.ss
@@ -1,8 +1,8 @@
 <% if EcommerceRecommendedProducts %>
 <div id="EcommerceRecommendedProducts">
-	<h3><% _t("YOUMAYALSO", "You may also be interested in the following products") %></h3>
-	<ul class="productList">
-		<% loop EcommerceRecommendedProducts %><% include Sunnysideup\EcommerceAlsoRecommended\IncludesProductGroupItem %><% end_loop %>
-	</ul>
+    <h3><% _t("YOUMAYALSO", "You may also be interested in the following products") %></h3>
+    <ul class="productList">
+        <% loop EcommerceRecommendedProducts %><% include Sunnysideup\EcommerceAlsoRecommended\IncludesProductGroupItem %><% end_loop %>
+    </ul>
 </div>
 <% end_if %>
